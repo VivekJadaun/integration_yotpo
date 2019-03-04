6640c674d3fa9577824733930d385544f4c2431d
VivekJadaun <vivekjadaun08@gmail.com>
2019-03-04 18:58:03 +0530

    module registered

---
+++ Model/Config/Backend/ValidateSettings.php
@@ -0,0 +1,3 @@
+<?php 
+
+namespace Integration\Yotpo\Model\Config\Backend;  
---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Integration_Yotpo',
+        __DIR__
+    );
+?>
